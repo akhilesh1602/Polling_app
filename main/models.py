b4495014e5e317be5c15290893856ee6688071e3
Akhilesh <akhilesh.verma1602@gmail.com>
2023-04-30 11:54:10 +0530

    completed the POST request from user

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Question(models.Model):
     slug = models.SlugField()
@@ -11,5 +13,13 @@ class Choice(models.Model):
     content = models.CharField(max_length=256)
     def __str__(self):
         return "{} - {}".format(self.question.content[:50], self.content)
+
+class Answer(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    choice = models.ForeignKey('Choice', on_delete= models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
 
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -1,15 +1,36 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import (
     ListView,
-    DetailView
+    DetailView,
+    FormView,
 )
-from main import models
+from main import models, forms
+from django.views.generic.detail import SingleObjectMixin
 
 class Index(ListView):
     model = models.Question
     template_name = 'main/index.html'
     # context_object_name = 'question_list' (lowercase modelname)
 
-class Question(DetailView):
+class Question(SingleObjectMixin, FormView):
     model = models.Question
     template_name = 'main/question.html'
+    form_class = forms.AnswerForm
+
+    def form_valid(self, form):
+        obj = form.save(commit = False)
+        obj.question = self.get_object()
+        obj.user = self.request.user
+        obj.save()
+        return HttpResponseRedirect('/')
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object = self.object)
+        return self.render_to_response(context)
